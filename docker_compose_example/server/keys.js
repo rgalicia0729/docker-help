8d139c7e389e55cedab28b2ad0d4d3dca107ee43
Rigo Galicia <rigo.galicia@icloud.com>
2022-01-25 13:05:17 -0600

    Se agrego un ejemplo de uso de docker compose

---
+++ docker_compose_example/server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pgHost: process.env.PGHOST,
+  pgUser: process.env.PGUSER,
+  pgPassword: process.env.PGPASSWORD,
+  pgDatabase: process.env.PGDATABASE,
+  pgPort: process.env.PGPORT,
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+};
